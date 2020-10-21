95db772bdc082f6ad57543dcb956e6b96ea60f6c
Thinkman Wang <wangxf1985@gmail.com>
2020-10-21 15:24:53 +0800

    update

---
+++ src/main/java/com/thinkman/thinkwebflux/ThinkwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.thinkman.thinkwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkwebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkwebfluxApplication.class, args);
+    }
+
+}
